96155e065a8677522a4c4d7af48249618a53f3c3
Ian Mancini <ianmethyst@gmail.com>
2023-03-08 11:36:33 -0300

    print json errors in donwloader

--- downloader.lua
+++ downloader.lua
@@ -6,7 +6,10 @@ function main()
     local raw_json, json_code = http.get(server_address .. '/processed/random')
     if not raw_json then error(json_code) end
 
+    print(raw_json)
     local json = JSON:decode(raw_json)
+
+    if not json then error("Couldn't parse json") end
 
     if not json.url then error("No url found") end
 
